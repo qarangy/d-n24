7ce2c3b54fa4d661bb78f7cebf305a353f13a448
qarangy <qarangy@gmail.com>
2023-07-12 22:54:55 +0500

    add js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const elModal = document.querySelector('.modal');
+const elModalClose = elModal.querySelector('.js-modal-close')
+
+if (elModalClose) {
+  elModalClose.addEventListener('click', function () {
+    elModal.classList.remove('modal-open');
+  });
+}
+
+setTimeout(function() {
+  elModal.classList.add('modal-open');
+}, 20000)
